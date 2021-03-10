30184876726f01a20bc086e78561ade23a2ed416
doannucphys <doan.phys@gmail.com>
2021-03-10 18:58:31 +0100

    update

--- release.sh
+++
@@ -1,3 +0,0 @@
-docker build -t 869969654446.dkr.ecr.eu-west-1.amazonaws.com/medika-frontend:v1 -f Dockerfile-release .
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 869969654446.dkr.ecr.eu-west-1.amazonaws.com
-docker push 869969654446.dkr.ecr.eu-west-1.amazonaws.com/medika-frontend:v1
--- run.sh
+++
@@ -1,3 +0,0 @@
-docker rm -f medika-frontend
-docker build -t medika/medika-frontend -f Dockerfile-dev .
-docker run -p 8080:8080 -d --env-file .env --name medika-frontend medika/medika-frontend:latest
